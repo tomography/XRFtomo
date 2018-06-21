18d114981b9fa2d108119026b2b1546af77d6751
Francesco De Carlo <decarlof@gmail.com>
2018-06-20 20:44:23 -0500

    update

--- read.py
+++ read.py
@@ -27,8 +27,6 @@ def restricted_float(x):
         raise argparse.ArgumentTypeError("%r not in range [0.0, 1.0]"%(x,))
     return x
 
-       
-
 def find_index(a_list, element):
     try:
         return a_list.tolist().index(element)
@@ -38,15 +36,13 @@ def find_index(a_list, element):
 def read_elements(h5fname):
     return(dxchange.read_hdf5(h5fname, "MAPS/channel_names"))
 
-def read_projection(fname, element):
+def read_projection(fname, element, theta_index):
 
     projections = dxchange.read_hdf5(fname, "MAPS/XRF_roi")
+    theta = dxchange.read_hdf5(fname, "MAPS/extra_pvs_as_csv")[theta_index].split(",")[1]
     elements = read_elements(fname)
-    print (elements)
-    print (projections.shape)
-    print(element, find_index(elements, element))
 
-#print ("ERROR: this element is not in the file")
+    return theta, projections[find_index(elements, element)]
 
 def main(arg):
 
@@ -55,27 +51,14 @@ def main(arg):
     parser.add_argument("--start", nargs='?', type=float, default=0.0, help="Angle first projection (default 0.0)")
     parser.add_argument("--end", nargs='?', type=float, default=180.0, help="Angle last projection (default 180.0)")
     parser.add_argument("--element", nargs='?', type=str, default="Si", help="Select the element to recontruct (default Si)")
-    parser.add_argument("--axis", nargs='?', type=str, default="0", help="Rotation axis location (pixel): 1024.0 (default 1/2 image horizontal size)")
-    parser.add_argument("--bin", nargs='?', type=int, default=0, help="Reconstruction binning factor as power(2, choice) (default 0, no binning)")
-    parser.add_argument("--method", nargs='?', type=str, default="gridrec", help="Reconstruction algorithm: sirtfbp (default gridrec)")
-    parser.add_argument("--type", nargs='?', type=str, default="slice", help="Reconstruction type: full, slice, try (default slice)")
-    parser.add_argument("--csw", nargs='?', type=int, default=10, help="+/- center search width (pixel): 10 (default 10). Search is in 0.5 pixel increments")
-    parser.add_argument("--nsino", nargs='?', type=restricted_float, default=0.5, help="Location of the sinogram to reconstruct (0 top, 1 bottom): 0.5 (default 0.5)")
 
     args = parser.parse_args()
 
     # Set path to the micro-CT data to reconstruct.
     fname = args.fname
-    algorithm = args.method
-    rot_center = float(args.axis)
-    binning = int(args.bin)
-
-    nsino = float(args.nsino)
     angle_start = float(args.start)
     angle_end = float(args.end)
     element = args.element
-    rec_type = args.type
-    center_search_width = args.csw
 
     if os.path.isfile(fname):    
 
@@ -83,7 +66,9 @@ def main(arg):
         for i, e in enumerate(elements):
             print ('%d:  %s' % (i, e))
         
-        read_projection(fname, element)
+        proj, theta = read_projection(fname, element, 657)
+        print ("theta:", theta)
+        print (proj.shape)
 
     elif os.path.isdir(fname):
         # Add a trailing slash if missing
@@ -91,18 +76,9 @@ def main(arg):
 
         h5_file_list = list(filter(lambda x: x.endswith(('.h5', '.hdf')), os.listdir(top)))
 
-        print(len(h5_file_list))
-        print((angle_end - angle_start)/len(h5_file_list))
-
-        # Create theta
-        theta = []
-        for i, f in enumerate(h5_file_list):
-            #print(i*(angle_end - angle_start)/len(h5_file_list))
-
-            angle = i*(angle_end - angle_start)/len(h5_file_list) 
-            #print ('%d:  %s at %d' % (i, f, theta[i]))
-            theta.append(angle)
-            print ('%d, %f deg: %s' % (i, theta[i], f))
+        for i, fname in enumerate(h5_file_list):
+            theta, proj = read_projection(top+fname, element, 657)
+            print(i, theta, fname)
 
     else:
         print("Directory or File Name does not exist: ", fname)
