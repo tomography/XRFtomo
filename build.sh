c330708b2d007379c542553c3ed04131e58b5a6b
Fabricio Marin <marinf@fsmarin.xray.aps.anl.gov>
2020-04-01 17:11:32 -0500

    adding json files for menu shortcuts using menuinst.

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-$PYTHON setup.py install
+mkdir -p "$PREFIX/Menu"
+if [ $OSX_ARCH ]
+then
+    cp "$RECIPE_DIR/menu-osx.json" "$PREFIX/Menu"
+    cp "$RECIPE_DIR/app.icns" "$PREFIX/Menu"
+else
+    cp "$RECIPE_DIR/menu-linux.json" "$PREFIX/Menu"
+    cp "$RECIPE_DIR/app.svg" "$PREFIX/Menu"
+fi
+
+$PYTHON setup.py install || exit 1
 
 # Add more build steps here, if they are necessary.
 
