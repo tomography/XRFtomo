26edbdacf48e12401537f96a6643381ab0174a16
unknown <fabricio.s.marin@gmail.com>
2020-08-04 21:42:06 -0500

    modified windows portion of the menu installer script.

--- xrftomo/menu_installer.py
+++ xrftomo/menu_installer.py
@@ -129,7 +129,7 @@ def install_menu():
 		subprocess.call(["open","-R",appPath])
 
 
-	elif plat == "Win":
+	elif plat == "win32" or plat == "windows":
 
 		'''
 		*makeBat: Create XRFtomo Batch File*
@@ -152,89 +152,92 @@ def install_menu():
 		@echo                XRFtomo
 		@echo ========================================================================
 		@
-		{:s}{:s} {:s} {:s}"%~1"
+		{:s} & {:s} {:s} {:s}"%~1"
 		@REM To keep the window from disappearing with any error messages
 		pause
 
 		'''
 
-		if __name__ == '__main__':
-			try:
-				import _winreg as winreg
-			except ImportError:
-				import winreg
-			app = None # delay starting wx until we need it. Likely not needed.
-			scriptpath = os.path.split(sys.argv[0])[0]
-			if not scriptpath: scriptpath = os.path.curdir                      #if no path specified: "", scriptpath="."
-			scriptpath = os.path.abspath(os.path.expanduser(scriptpath))        #scriptpath = =current path
-			XRFscript = os.path.join(scriptpath,'__main__.py')                   #assuming path is where script is
-			XRFbat = os.path.join(scriptpath,'RunXRFtomo.bat')                   #place bat alongside xrftomo ?
-			XRFicon = os.path.join(scriptpath,'xrftomo.ico')                     #place xrftomo.ico alongisde xrftomo.py ?
-			pythonexe = os.path.realpath(sys.executable)                        #python path, automatically detects python path
-			print('Python installed at',pythonexe)
-			print('xrftomo installed at',scriptpath)
-			# Bob reports a problem using pythonw.exe w/Canopy on Windows, so change that if used
-			if pythonexe.lower().endswith('pythonw.exe'):
-				print("  using python.exe rather than "+pythonexe)
-				pythonexe = os.path.join(os.path.split(pythonexe)[0],'python.exe')
-				print("  now pythonexe="+pythonexe)
+		try:
+			import _winreg as winreg
+		except ImportError:
+			import winreg
+		app = None # delay starting wx until we need it. Likely not needed.
+		scriptpath = os.path.split(sys.argv[0])[0]
+		if not scriptpath: scriptpath = "/".join(os.path.abspath("xrftomo.__file__").split("/")[:-1])+"/"
+                      #if no path specified: "", scriptpath="."
+		scriptpath = os.path.abspath(os.path.expanduser(scriptpath))        #scriptpath = =current path
+		XRFscript = os.path.join(scriptpath,'__main__.py')                   #assuming path is where script is
+		XRFbat = os.path.join(scriptpath,'RunXRFtomo.bat')                   #place bat alongside xrftomo ?
+		XRFicon = os.path.join(scriptpath,'xrftomo.ico')                     #place xrftomo.ico alongisde xrftomo.py ?
+		pythonexe = os.path.realpath(sys.executable)                        #python path, automatically detects python path
+		# if pythonExe.split("/")[-1] == "python3.6":
+		# 	pythonExe = "/".join(pythonExe.split("/")[:-1])+"/python"
+		print('Python installed at',pythonexe)
+		print('xrftomo installed at',scriptpath)
+		# Bob reports a problem using pythonw.exe w/Canopy on Windows, so change that if used
+		if pythonexe.lower().endswith('pythonw.exe'):
+			print("  using python.exe rather than "+pythonexe)
+			pythonexe = os.path.join(os.path.split(pythonexe)[0],'python.exe')
+			print("  now pythonexe="+pythonexe)
 
-			# create a GSAS-II script
-			fp = open(os.path.join(XRFbat),'w')
-			fp.write("@REM created by run of bootstrap.py on {:%d %b %Y %H:%M}\n".format(datetime.datetime.now()))
-			activate = os.path.join(os.path.split(pythonexe)[0],'Scripts','activate')
-			print("Looking for",activate)
-			if os.path.exists(activate):
-				activate = os.path.realpath(activate)
-				if ' ' in activate:
-					activate = 'call "'+ activate + '"\n'
-				else:
-					activate = 'call '+ activate + '\n'
-				print('adding activate to .bat file ({})'.format(activate))
+		# create a GSAS-II script
+		fp = open(os.path.join(XRFbat),'w')
+		fp.write("@REM created by run of bootstrap.py on {:%d %b %Y %H:%M}\n".format(datetime.datetime.now()))
+		activate = os.path.join(os.path.split(pythonexe)[0],'Scripts','activate')
+		print("Looking for",activate)
+		if os.path.exists(activate):
+			activate = os.path.realpath(activate)
+			if ' ' in activate:
+				activate = 'call "'+ activate + '"\n'
 			else:
-				print('Anaconda activate not found')
-				activate = ''
-			pexe = pythonexe
-			if ' ' in pythonexe:
-				pexe = '"'+pythonexe+'"'
-			XRFs = XRFscript
-			if ' ' in XRFs:
-				XRFs = '"'+XRFscript+'"'
+				activate = 'call '+ activate + '\n'
+			print('adding activate to .bat file ({})'.format(activate))
+		else:
+			AnacondaPathIndx = os.path.split(pythonexe)[0].split("\\").index("Anaconda3")
+			activate = "\\".join(os.path.split(pythonexe)[0].split("\\")[:AnacondaPathIndx+1])+"\\Scripts\\activate py36"
+			print("set activate path to {}".format(activate))
+		pexe = pythonexe
+		if ' ' in pythonexe:
+			pexe = '"'+pythonexe+'"'
+		XRFs = XRFscript
+		if ' ' in XRFs:
+			XRFs = '"'+XRFscript+'"'
 
-			args = 'gui'
-			fp.write(Script.format(activate,pexe,XRFs,args))
-			fp.close()
-			print('\nCreated xrftomo batch file xrftomo.bat in '+scriptpath)
+		args = 'gui'
+		fp.write(Script.format(activate,pexe,XRFs,args))
+		fp.close()
+		print('\nCreated xrftomo batch file xrftomo.bat in '+scriptpath)
 
-			new = False
-			oldBat = ''
-			try:
-				FileNotFoundError
-			except NameError:
-				FileNotFoundError = Exception
+		new = False
+		oldBat = ''
+		try:
+			FileNotFoundError
+		except NameError:
+			FileNotFoundError = Exception
 
-			try:
-				import win32com.shell.shell, win32com.shell.shellcon, win32com.client
-				desktop = win32com.shell.shell.SHGetFolderPath(
-					0, win32com.shell.shellcon.CSIDL_DESKTOP, None, 0)
-				shortbase = "xrftomo.lnk"
-				shortcut = os.path.join(desktop, shortbase)
-				save = True
+		try:
+			import win32com.shell.shell, win32com.shell.shellcon, win32com.client
+			desktop = win32com.shell.shell.SHGetFolderPath(
+				0, win32com.shell.shellcon.CSIDL_DESKTOP, None, 0)
+			shortbase = "xrftomo.lnk"
+			shortcut = os.path.join(desktop, shortbase)
+			save = True
 
-				if save:
-					shell = win32com.client.Dispatch('WScript.Shell')
-					shobj = shell.CreateShortCut(shortcut)
-					shobj.Targetpath = XRFbat
-					#shobj.WorkingDirectory = wDir # could specify a default project location here
-					shobj.IconLocation = XRFicon
-					shobj.save()
-					print('Created shortcut to start xrftomo on desktop')
-				else:
-					print('No shortcut for this xrftomo created on desktop')
+			if save:
+				shell = win32com.client.Dispatch('WScript.Shell')
+				shobj = shell.CreateShortCut(shortcut)
+				shobj.Targetpath = XRFbat
+				#shobj.WorkingDirectory = wDir # could specify a default project location here
+				shobj.IconLocation = XRFicon
+				shobj.save()
+				print('Created shortcut to start xrftomo on desktop')
+			else:
+				print('No shortcut for this xrftomo created on desktop')
 
-			except ImportError:
-				print('Module pywin32 not present, will not make desktop shortcut')
-			except:
-				print('Unexpected error making desktop shortcut. Please report:')
-				import traceback
-				print(traceback.format_exc())
+		except ImportError:
+			print('Module pywin32 not present, will not make desktop shortcut')
+		except:
+			print('Unexpected error making desktop shortcut. Please report:')
+			import traceback
+			print(traceback.format_exc())
