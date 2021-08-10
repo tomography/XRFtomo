3a977bbd4a7372a2147940b55d89c8d45670e0c1
Fabricio Marin <fabricio.s.marin@gmail.com>
2021-08-10 15:21:06 -0500

    menu installer not finding correct directories when installed from conda, attempting fix

--- xrftomo/menu_installer.py
+++ xrftomo/menu_installer.py
@@ -162,21 +162,34 @@ def install_menu():
 			import _winreg as winreg
 		except ImportError:
 			import winreg
+		import xrftomo 
+		xrftomo_path = xrftomo.__file__
+		pkg_name = xrftomo_path.split(".egg")[0].split("\\")[-1].replace(".","_")
+		src_dir = xrftomo_path.split("envs")[0]+"pkgs"+pkg_name+"\\info\\recipe"
+		menu_dir = src_dir+"\\Menu\\"
+		entry_point = os.popen("which xrftomo")
+		entry_point = entry_point.read().split("\n")[0]
+
+
 		app = None # delay starting wx until we need it. Likely not needed.
 		#scriptpath = os.path.split(sys.argv[0])[0]
-		current_path = os.path.abspath(os.path.expanduser("menu_installer.py"))
-		print("this is the path", current_path)
-		scriptpath = "\\".join(current_path.split("\\")[:-1])+"\\"
+		# current_path = os.path.abspath(os.path.expanduser("menu_installer.py"))
+		# print("this is the path", current_path)
+		print("this is the path", src_dir)
+		# scriptpath = "\\".join(current_path.split("\\")[:-1])+"\\"
+		scriptpath = src_dir+"\\xrftomo\\"
 		print("this is the scrpt path", scriptpath)
 
 		#if no path specified: "", scriptpath="."
-		scriptpath = os.path.abspath(os.path.expanduser(scriptpath))        #scriptpath = =current path
+		# scriptpath = os.path.abspath(os.path.expanduser(scriptpath))        #scriptpath = =current path
 		XRFscript = os.path.join(scriptpath,'__main__.py')                   #assuming path is where script is
 		XRFbat = os.path.join(scriptpath,'RunXRFtomo.bat')                   #place bat alongside xrftomo ?
-		XRFicon = os.path.join(scriptpath,'xrftomo.ico')                     #place xrftomo.ico alongisde xrftomo.py ?
+		# XRFicon = os.path.join(scriptpath,'xrftomo.ico')                     #place xrftomo.ico alongisde xrftomo.py ?
+		XRFicon = os.path.join(menu_dir,'xrftomo.ico')                     #place xrftomo.ico alongisde xrftomo.py ?
 		pythonexe = os.path.realpath(sys.executable)                        #python path, automatically detects python path
 		print('Python installed at',pythonexe)
-		print('xrftomo installed at',scriptpath)
+		# print('xrftomo installed at',scriptpath)
+		print('xrftomo installed at',entry_point)
 		# Bob reports a problem using pythonw.exe w/Canopy on Windows, so change that if used
 		if pythonexe.lower().endswith('pythonw.exe'):
 			print("  using python.exe rather than "+pythonexe)
