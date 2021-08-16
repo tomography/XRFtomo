commented out line normalization line due to a bug and converted projection arrays to float32 before saving.

xrftomo/file_io/writer.py
@@ -132,9 +132,9 @@ class SaveOptions(object):
 				os.makedirs(path)
 				for i in range(data.shape[1]):		#angle index
 					img = data[j, i, :, :]
-					# temp = Image.fromarray(img.astype(np.float32))
-					# temp.save(path+"/"+element_names[j]+"_"+str(i)+'_'+fnames[0].split(".")[0]+".tif")
-					io.imsave(path+"/"+element_names[j]+"_"+str(i)+'_'+fnames[i].split(".")[0]+".tif", img)
+					img = img.astype(np.float32)
+					# temp.save(path+"/"+element_names[j]+"_"+str(i)+'_'+fnames[0].split(".")[0]+".tiff")
+					io.imsave(path+"/"+element_names[j]+"_"+str(i)+'_'+fnames[i].split(".")[0]+".tiff", img)
 			return
 		except IOError:
 			print("type the header name")
@@ -154,7 +154,7 @@ class SaveOptions(object):
 				recon = tomopy.circ_mask(recon, axis=0)
 				indx = "0000"
 				recon_index = indx[:-len(str(index))]+str(index)
-				io.imsave(savedir+"_"+str(recon_index)+".tif", recon[0])
+				io.imsave(savedir+"_"+str(recon_index)+".tiff", recon[0])
 			return
 		except IOError:
 			print("type the header name")
@@ -201,8 +201,8 @@ class SaveOptions(object):
 
 			os.makedirs(savedir)
 			# temp_img = Image.fromarray(sinodata.astype(np.float32))
-			# temp_img.save(savedir + "/" + "sinogram.tif")
-			io.imsave(savedir + "/" + "sinogram.tif", sinodata)
+			# temp_img.save(savedir + "/" + "sinogram.tiff")
+			io.imsave(savedir + "/" + "sinogram.tiff", sinodata)
 			return
 			
 		except IOError:
@@ -228,8 +228,8 @@ class SaveOptions(object):
 			for i in range(num_elements):
 				element = element_names[i]
 				# temp_img = Image.fromarray(sinogramData[i].astype(np.float32))
-				# temp_img.save(savedir + "/"+element+"_sinogram.tif")
-				io.imsave(savedir + "/"+element+"_sinogram.tif", sinogramData[i])
+				# temp_img.save(savedir + "/"+element+"_sinogram.tiff")
+				io.imsave(savedir + "/"+element+"_sinogram.tiff", sinogramData[i])
 			return
 
 		except IOError:

xrftomo/widgets/file_loader.py
@@ -661,7 +661,8 @@ class FileTableWidget(QtWidgets.QWidget):
         if data is None or scalers is None:
             return [], [], [], []
         # if self.quant_options.currentText() != 'None':
-        self.data = self.normalizeData(data, scalers)
+        # TODO: work on nromalizeData function, it removes real data. or put in a conditional statment to use it only when normalization is necessary.
+        # self.data = self.normalizeData(data, scalers)
         self.message.setText('finished loading')
 
         data[np.isnan(data)] = 0.0001