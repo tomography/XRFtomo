a8dcdc5353d9f67836b6de0c087077795f400fea
FabricioSMarin <fabricio.s.marin@gmail.com>
2023-12-13 14:51:40 -0600

    modified bat file to start desktop shortcut with cmd window minimized.

--- Menu/start_xrftomo.bat
+++ Menu/start_xrftomo.bat
@@ -1,2 +1,1 @@
-@echo on
-C:\ProgramData\anaconda3\Scripts\conda.exe run -n py310 python -u C:\Users\marinf\conda\XRFtomo\xrftomo\__main__.py gui
+start /min C:\ProgramData\anaconda3\Scripts\conda.exe run -n py310 python -u C:\Users\marinf\conda\XRFtomo\xrftomo\__main__.py gui --experimental
